ea179b38e9b2729fc032e0c53507ad887c87de41
Kuznix <krzysztofdemirkuzniak@gmail.com>
2026-09-25 18:53:19 +0200

    Remove obsolete root component files

--- LH4051-SESSION.h
+++
@@ -1,7 +0,0 @@
-#ifndef LH4051_SESSION_H
-#define LH4051_SESSION_H
-#include <gtk/gtk.h>
-void lh4051_session_start(GtkApplication *app);
-void lh4051_session_stop(void);
-GtkApplication *lh4051_session_app(void);
-#endif
