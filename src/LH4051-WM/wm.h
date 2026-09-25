582384a1e2bee9794a150a16864bc34dc2bdefa1
Kuznix <krzysztofdemirkuzniak@gmail.com>
2026-09-25 19:51:35 +0200

    Add backend-aware WM API

--- src/LH4051-WM/wm.h
+++ src/LH4051-WM/wm.h
@@ -1,9 +1,12 @@
 #ifndef LH4051_WM_H
 #define LH4051_WM_H
 #include <gtk/gtk.h>
+
 typedef void (*LH4051WMChangedFunc)(void *user_data);
+
 void lh4051_wm_init(void);
 void lh4051_wm_shutdown(void);
+void lh4051_wm_set_backend(GdkDisplay *display);
 void lh4051_wm_register(GtkWindow *window, const char *title);
 void lh4051_wm_unregister(GtkWindow *window);
 void lh4051_wm_set_title(GtkWindow *window, const char *title);
@@ -12,4 +15,5 @@ GtkWindow *lh4051_wm_get_window(guint index);
 const char *lh4051_wm_get_title(guint index);
 void lh4051_wm_activate(GtkWindow *window);
 void lh4051_wm_set_changed_callback(LH4051WMChangedFunc callback, void *user_data);
+const char *lh4051_wm_backend_name(void);
 #endif
