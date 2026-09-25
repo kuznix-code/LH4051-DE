57529da87bdd2af7cdec0a54e0c43390960a4e17
Kuznix <krzysztofdemirkuzniak@gmail.com>
2026-09-25 18:32:11 +0200

    Major 0.0.0 desktop shell overhaul

--- src/longhorn.h
+++ src/longhorn.h
@@ -1,10 +1,17 @@
 #ifndef LONGHORN_H
 #define LONGHORN_H
-
 #include <gtk/gtk.h>
 #include <gtk4-layer-shell.h>
-
+#define LH4051_VERSION "0.0.0"
+#define LH4051_PANEL_HEIGHT 42
+#define LH4051_SIDEBAR_WIDTH 280
+void load_shell_css(void);
+void create_desktop(GtkApplication *app);
 void create_longhorn_panel(GtkApplication *app);
 void create_longhorn_sidebar(GtkApplication *app);
-
+void create_start_menu(GtkApplication *app);
+void create_file_explorer(GtkApplication *app, const char *path);
+void longhorn_show_start_menu(GtkApplication *app);
+void longhorn_hide_start_menu(void);
+void longhorn_toggle_start_menu(GtkApplication *app);
 #endif
