57217bdbbbd041f95762580408d753fa1b47d486
Kuznix <krzysztofdemirkuzniak@gmail.com>
2026-09-25 19:51:30 +0200

    Overhaul session backend API for Wayland and X11

--- src/LH4051-SESSION/session.h
+++ src/LH4051-SESSION/session.h
@@ -1,7 +1,19 @@
 #ifndef LH4051_SESSION_H
 #define LH4051_SESSION_H
 #include <gtk/gtk.h>
+#include <gtk4-layer-shell.h>
+
+typedef enum {
+    LH4051_SHELL_DESKTOP,
+    LH4051_SHELL_PANEL,
+    LH4051_SHELL_SIDEBAR,
+    LH4051_SHELL_POPUP
+} LH4051ShellRole;
+
 void lh4051_session_start(GtkApplication *app);
 void lh4051_session_stop(void);
 GtkApplication *lh4051_session_app(void);
-#endif
+gboolean lh4051_session_is_wayland(void);
+gboolean lh4051_session_is_x11(void);
+const char *lh4051_session_backend_name(void);
+void lh4051_session_setup_shell_window(GtkWindow *window, LH4051ShellRole role);
