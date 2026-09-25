dfccdacad31389242982486cb8e3ea5c13666722
Kuznix <krzysztofdemirkuzniak@gmail.com>
2026-09-25 19:40:44 +0200

    Redesign file manager and add cross-platform file opening

--- src/LH4051-FM/fm.h
+++ src/LH4051-FM/fm.h
@@ -1,6 +1,21 @@
 #ifndef LH4051_FM_H
 #define LH4051_FM_H
+
+#include <gio/gio.h>
 #include <gtk/gtk.h>
+
+typedef struct {
+    char *name;
+    char *path;
+    GFileType type;
+    goffset size;
+    char *content_type;
+    gint64 modified;
+} LH4051FMItem;
+
 GPtrArray *lh4051_fm_list(const char *path);
 void lh4051_fm_free_list(GPtrArray *items);
+LH4051FMItem *lh4051_fm_item_new(const char *path, const char *name);
+void lh4051_fm_item_free(LH4051FMItem *item);
+
 #endif
